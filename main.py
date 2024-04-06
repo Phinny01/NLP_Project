d1879a6b2aec1e60664b7a30972be23f6959cda8
Donald Echefu <55097668+Donald-ED@users.noreply.github.com>
2024-04-05 21:48:47 -0400

    Making new branch and creating disease filters

--- main.py
+++ main.py
@@ -6,8 +6,16 @@ from io import StringIO
 data = pd.read_csv('training_congruent.csv')
 
 # Filtering for positive cases of Pleural Effusion and Pneumonia
-positive_cases = data[(data['Pleural Effusion'] == 1) | (data['Pneumonia'] == 1)]
+positive_cases_pf = data[(data['Pleural Effusion'] == 1)]
+positive_cases_pn = data[(data['Pneumonia'] == 1)]
+no_findings = data[(data['No_Findings'] == 1)]
+positive_cases_pt = data[(data['Pneumothorax'] == 1)]
+positive_cases_at = data[(data['Atelectasis'] == 1)]
 
 # Printing the study_id of positive cases
 print("Study IDs for positive cases of Pleural Effusion and Pneumonia:")
-print(positive_cases['study_id'].to_list())
+print(52823782.0 in positive_cases_pn['study_id'].values)
+print(52823782.0 in positive_cases_pf['study_id'].values)
+
+
+
